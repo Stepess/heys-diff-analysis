2622e17eb0e87af544435f03dcc6013f95f22dd4
Stepess <stepanersh@gmail.com>
2020-06-10 14:21:29 +0300

    somehow diffs works

--- heys-cipher/src/main/java/ua/stepess/crypto/diff/DifferentialSearch.java
+++ heys-cipher/src/main/java/ua/stepess/crypto/diff/DifferentialSearch.java
@@ -6,7 +6,6 @@ import ua.stepess.util.HeysCipherFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,31 +20,28 @@ public class DifferentialSearch {
     public static final String DEFAULT_KEY = "a1722101d9e038caa7b4d120c18b";
 
     public static void main(String[] args) throws IOException {
-        /*int[] alphas = {0xf000, 0x0f00, 0x00f0, 0x000f,
+        int[] alphas = {0xf000, 0x0f00, 0x00f0, 0x000f,
                 0x1000, 0x0100, 0x0010, 0x0001,
                 0x2000, 0x0200, 0x0020, 0x0002,
                 0x3000, 0x0300, 0x0030, 0x0003,
-        };*/
-
-        /*int[] alphas = {0xe000, 0x0e00, 0x00e0, 0x000e,
+                0xe000, 0x0e00, 0x00e0, 0x000e,
                 0xa000, 0x0a00, 0x00a0, 0x000a,
                 0xb000, 0x0b00, 0x00b0, 0x000b,
                 0x7000, 0x0700, 0x0070, 0x0007,
                 0xd000, 0x0d00, 0x00d0, 0x000d,
                 0xc000, 0x0c00, 0x00c0, 0x000c,
-        };*/
+        };
 
-        var differentials = new HashMap<Integer, List<Integer>>();
+        //var differentials = new HashMap<Integer, List<Integer>>();
+        var differentials = new HashMap<Integer, Map<Integer, Double>>();
 
-        int[] alphas = {0x0100};
         for (int alpha : alphas) {
-            var searchResult = search(alpha, 6);
+            var searchResult = search(alpha, 5);
 
-            var diffs = new ArrayList<>(searchResult.keySet());
-            differentials.put(alpha, diffs);
-
-            writeToDisk(alpha, searchResult);
+            differentials.put(alpha, searchResult);
         }
+
+        writeToDisk(differentials);
 
         /*var ciphertext = new HashMap<Integer, Integer>();
 
@@ -59,9 +55,8 @@ public class DifferentialSearch {
 
     }
 
-    private static void writeToDisk(int alpha, Map<Integer, Double> searchResult) throws IOException {
-        var fileName = String.format("alpha_%s.json", Integer.toHexString(alpha));
-        var file = new File(fileName);
+    private static void writeToDisk(Map<Integer, Map<Integer, Double>> searchResult) throws IOException {
+        var file = new File("differentials.json");
 
         OBJECT_MAPPER.writeValue(file, searchResult);
     }
@@ -71,7 +66,7 @@ public class DifferentialSearch {
         previous.put(alpha, 1.0);
 
         // the last one should be >> 0.00003051757
-        double[] bounds = {0.0001, 0.00000013, 0.00000000006, 0.00000000000007, 0.00000000000000002, 0.0005};
+        double[] bounds = {0.001, 0.00013, 0.0009, 0.00007, 0.001};
 
         Map<Integer, Double> current = new HashMap<>();
 
@@ -83,12 +78,7 @@ public class DifferentialSearch {
                 var probabilities = calculateProbabilities(g.getKey());
 
                 for (int x = 0; x < VECTORS_NUM; x++) {
-                    var p = current.get(x);
-                    if (p != null) {
-                        current.put(x, p + probabilities[x] * g.getValue());
-                    } else {
-                        current.put(x, probabilities[x] * g.getValue());
-                    }
+                    current.put(x, current.getOrDefault(x, 0.0) + probabilities[x] * g.getValue());
                 }
             }
 
@@ -96,10 +86,12 @@ public class DifferentialSearch {
 
             for (Map.Entry<Integer, Double> pair : current.entrySet()) {
                 if (pair.getValue() > bounds[i]) {
+                //if (pair.getValue() > (1.0 / (1 << 10))) {
                     previous.put(pair.getKey(), pair.getValue());
                 }
             }
 
+            System.out.println();
             System.out.println("Round #" + i);
             System.out.println();
             System.out.println("Survived : " + previous);
@@ -141,10 +133,8 @@ public class DifferentialSearch {
 
         // this cipher is Markov's, so differential probabilities doesn't depends on input
         for (int k = 0; k < VECTORS_NUM; k++) {
-            frequencies[HEYS.doEncryptionRound(0, k) ^
-                    HEYS.doDecryptionRound(alpha, k)]++;
+            frequencies[HEYS.doEncryptionRound(0, k) ^ HEYS.doEncryptionRound(alpha, k)]++;
         }
-
 
         double[] probabilities = new double[VECTORS_NUM];
 
