9bbbf4196e20bce18da97d426d81249325ce3411
Stepess <stepanersh@gmail.com>
2020-06-21 13:44:19 +0300

    refactor

--- heys-cipher/src/main/java/ua/stepess/crypto/linear/LinearAttack.java
+++ heys-cipher/src/main/java/ua/stepess/crypto/linear/LinearAttack.java
@@ -13,13 +13,11 @@ public class LinearAttack {
 
     public static final int VECTORS_NUM = 0x10000;
 
-    private static final int LAST_KEYS_NUMBER = 16;
+    private static final int KEY_LIMIT = 10;
 
-    private static final int KEYS_NUMBER = 100;
+    private static final int PER_APPROXIMATION_KEY_LIMIT = 100;
 
     private static final HeysCipher CIPHER = (HeysCipher) HeysCipherFactory.getDefaultHeysCipher();
-
-    private static final double[][] S_BOX_LP = computeLinearPotentials();
 
     public static void main(String[] args) {
         generateApproximations();
@@ -39,7 +37,7 @@ public class LinearAttack {
 
         System.out.println("A = " + approximations.size());
 
-        Map<Integer, Integer> recoverFirst = recoverFirst(data, approximations, keys[0]);
+        var recoverFirst = findMostProbableKeyForApproximations(data, approximations, keys[0]);
 
         recoverFirst.forEach((k, c) -> System.out.println("Key = " + Integer.toHexString(k) + " count = " + c));
     }
@@ -86,7 +84,7 @@ public class LinearAttack {
             current.clear();
 
             for (Map.Entry<Integer, Double> g : previous.entrySet()) {
-                var probabilities = differenceProbabilityDistribution(g.getKey());
+                var probabilities = calculateProbabilities(g.getKey());
 
                 for (int x = 0; x < VECTORS_NUM; x++) {
                     current.put(x, current.getOrDefault(x, 0.0) + probabilities[x] * g.getValue());
@@ -102,128 +100,93 @@ public class LinearAttack {
             }
         }
 
-        System.out.println("diffs = " + previous);
+        System.out.println("approximations = " + previous);
 
         return previous.entrySet().stream()
                 .map(e -> Approximation.of(alpha, e.getKey(), e.getValue()))
                 .collect(Collectors.toList());
     }
     
-    public static Map<Integer, Integer> recoverFirst(Map<Integer, Integer> data, Collection<Approximation> pairs, int rightKey) {
-        Map<Integer, Integer> counts = new HashMap<>();
-        for (Approximation approximation : pairs) {
+    public static Map<Integer, Integer> findMostProbableKeyForApproximations(Map<Integer, Integer> plaintextCiphertextMap, List<Approximation> approximations, int rightKey) {
+        Map<Integer, Integer> keyScore = new HashMap<>();
+        for (Approximation approximation : approximations) {
             System.out.println("Approximation: " + approximation);
-            for (Integer key : recoverFirst(data, approximation.a, approximation.b)) {
-                counts.put(key, counts.getOrDefault(key, 0) + 1);
-            }
-            head(counts, LAST_KEYS_NUMBER).forEach((k,v) ->
+            var mostProbableKeysForApproximation =
+                    findMostProbableKeysForApproximation(plaintextCiphertextMap, approximation.a, approximation.b);
+            mostProbableKeysForApproximation.forEach(key ->
+                    keyScore.put(key, keyScore.getOrDefault(key, 0) + 1));
+
+            findFirst(keyScore, KEY_LIMIT).forEach((k, v) ->
                     System.out.println("Key: " + Integer.toHexString(k) + " count: " + v));
 
             System.out.println();
             System.out.println();
             System.out.println();
 
-            System.out.println("Contains key: " + counts.containsKey(rightKey));
-            System.out.println("key count: " + counts.get(rightKey));
+            System.out.println("Contains key: " + keyScore.containsKey(rightKey));
+            System.out.println("key count: " + keyScore.get(rightKey));
 
             System.out.println();
             System.out.println();
             System.out.println();
             System.out.println();
         }
-        return head(counts, LAST_KEYS_NUMBER);
+        return findFirst(keyScore, KEY_LIMIT);
     }
 
-    public static Map<Integer, Integer> recoverFirst(Map<Integer, Integer> data, Collection<Approximation> pairs) {
-        Map<Integer, Integer> counts = new HashMap<>();
-        for (Approximation approximation : pairs) {
-            System.out.println("Approximation: " + approximation);
-            for (Integer key : recoverFirst(data, approximation.a, approximation.b)) {
-                counts.put(key, counts.getOrDefault(key, 0) + 1);
-            }
-            head(counts, LAST_KEYS_NUMBER).forEach((k,v) ->
-                    System.out.println("Key: " + Integer.toHexString(k) + " count: " + v));
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-        }
-        return head(counts, LAST_KEYS_NUMBER);
-    }
-
-    public static Collection<Integer> recoverFirst(Map<Integer, Integer> data, int a, int b) {
-        Map<Integer, Integer> counts = new HashMap<>(1 << 16);
-        int[][] dataArray = toArray(data);
-        for (int k = 0; k < (1 << HeysCipherFactory.BLOCK_SIZE); k++) {
+    public static Set<Integer> findMostProbableKeysForApproximation(Map<Integer, Integer> plaintextCiphertext, int a, int b) {
+        Map<Integer, Integer> counts = new HashMap<>(VECTORS_NUM);
+        for (int k = 0; k < VECTORS_NUM; k++) {
             int count = 0;
-            for (int t = 0; t < dataArray.length; t++) {
-                int x = CIPHER.doEncryptionRound(dataArray[t][0], k);
-                count += dot(a, x) ^ dot(b, dataArray[t][1]);
+            for (Map.Entry<Integer, Integer> p : plaintextCiphertext.entrySet()) {
+                int x = CIPHER.doEncryptionRound(p.getKey(), k);
+                count += scalarProduct(a, x) ^ scalarProduct(b, p.getValue());
             }
-            count = Math.max(count, (1 << HeysCipherFactory.BLOCK_SIZE) - count);
+            count = Math.max(count, VECTORS_NUM - count);
             counts.put(k, Math.abs(count));
         }
-        return head(counts, KEYS_NUMBER).keySet();
+        return findFirst(counts, PER_APPROXIMATION_KEY_LIMIT).keySet();
     }
 
-    public static double[] differenceProbabilityDistribution(int a) {
-        double[] distribution = new double[1 << HeysCipherFactory.BLOCK_SIZE];
+    public static double[] calculateProbabilities(int a) {
+        var linearPotentials = computeLinearPotentials();
+        var distribution = new double[VECTORS_NUM];
         for (int b = 0; b < distribution.length; b++) {
-            distribution[b] = getLp(a, b);
+            distribution[b] = 1.0;
+            b = CIPHER.shuffle(b);
+            for (int i = 0; i < HeysCipherFactory.N; i++) {
+                int aPrime = (a >>> (4 * i)) & 0xF;
+                int bPrime = (b >>> (4 * i)) & 0xF;
+                distribution[b] *= linearPotentials[aPrime][bPrime];
+            }
         }
         return distribution;
     }
 
-    private static double getLp(int a, int b) {
-        double p = 1.0;
-        b = CIPHER.shuffle(b);
-        for (int i = 0; i < HeysCipherFactory.N; i++) {
-            int aPrime = (a >>> (4 * i)) & 0xF;
-            int bPrime = (b >>> (4 * i)) & 0xF;
-            p *= S_BOX_LP[aPrime][bPrime];
+    public static int scalarProduct(int x, int y) {
+        int res = 0;
+        for (int i = 0; i < HeysCipherFactory.BLOCK_SIZE; i++) {
+            res ^= ((x >>> i) & (y >>> i));
         }
-        return p;
-    }
-
-    public static int dot(int x, int y) {
-        int z = x & y;
-        z = z ^ (z >>> 8);
-        z = z ^ (z >>> 4);
-        z = z ^ (z >>> 2);
-        return (z ^ (z >>> 1)) & 0x1;
+        return res & 0x1;
     }
 
     private static double[][] computeLinearPotentials() {
         double[][] lp = new double[1 << HeysCipherFactory.N][1 << HeysCipherFactory.N];
         for (int a = 0; a < (1 << HeysCipherFactory.N); a++) {
             for (int b = 0; b < (1 << HeysCipherFactory.N); b++) {
-                double val = 0;
+                double value = 0;
                 for (int x = 0; x < (1 << HeysCipherFactory.N); x++) {
-                    int degree = dot(a, x) ^ dot(b, CIPHER.substitute(x));
-                    if (degree == 1) {
-                        val--;
-                    } else {
-                        val++;
-                    }
+                    int degree = scalarProduct(a, x) ^ scalarProduct(b, CIPHER.substitute(x));
+                    value += degree == 1 ? -1 : 1;
                 }
-                lp[a][b] = Math.pow(val / (1 << HeysCipherFactory.N), 2);
+                lp[a][b] = Math.pow(value / (1 << HeysCipherFactory.N), 2);
             }
         }
         return lp;
     }
 
-    private static int[][] toArray(Map<Integer, Integer> data) {
-        int[][] array = new int[data.size()][2];
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : data.entrySet()) {
-            array[index][0] = entry.getKey();
-            array[index][1] = entry.getValue();
-            index++;
-        }
-        return array;
-    }
-
-    public static Map<Integer, Integer> head(Map<Integer, Integer> data, int size) {
+    public static Map<Integer, Integer> findFirst(Map<Integer, Integer> data, int size) {
         return data.entrySet()
                 .stream()
                 .sorted(Comparator.comparingInt(Map.Entry<Integer, Integer>::getValue).reversed())
